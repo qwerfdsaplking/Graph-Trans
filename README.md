9a56135114df6924d4ea2b01820797551fcf54d7
erxuemin <mex338@qq.com>
2022-03-13 21:27:19 +0800

    todo

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Graph-Trans
+
+
+TODO
--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from sklearn.metrics import roc_auc_score, average_precision_score
 import tensorflow.compat.v1 as tf
 tf.disable_v2_behavior()
 from argparse import ArgumentParser, Namespace
-
+from data.collator import *
 from gt_dataset import *
 
 import gc
